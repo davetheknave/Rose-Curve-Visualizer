0ca9f0ac376ff3f7dee1c57af848778f21393e75
David Stearns <knavedavid@gmail.com>
2023-11-18 21:19:51 -0500

    Make cosmetic changes and add color pickers.

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,30 @@ import { useState } from 'react'
 import P5Sketch from './rosecurve.jsx'
 import './App.css'
 
+function arrayToHex(input) {
+  return "#" + input[0].toString(16) + input[1].toString(16) + input[2].toString(16);
+}
 
-
-function Slider(props){
+function Slider(props) {
   return (
     <>
-      <label className="slider">
-        <span>{props.children}</span>
-        <input type="range" min={props.min ?? 1} max={props.max ?? 250} onInput={(v)=>props.change(v.target.value)}/>
-        <span>{props.displayValue}</span>
+      <label className="slider visualizer-input">
+        <span className="slider-label">{props.children}</span>
+        <input type="range" min={props.min ?? 1} max={props.max ?? 250} onInput={(v) => props.change(v.target.value)} />
+        <span className="slider-value">{props.displayValue}</span>
       </label>
     </>
   );
+}
+function ColorPicker(props) {
+  return (
+    <>
+      <label className="colorPicker visualizer-input">
+        <span>{props.children}</span>
+        <input type="color" onInput={(v) => props.change(v.target.value)} value={props.initialValue} />
+      </label>
+    </>
+  )
 }
 
 function App() {
@@ -21,16 +33,26 @@ function App() {
   const [denominator, setDenominator] = useState(9)
   const [spacing, setSpacing] = useState(250)
   const [speed, setSpeed] = useState(10)
+  const [background, setBG] = useState([18, 18, 18])
+  const [foreground, setFG] = useState([200, 200, 250])
 
   return (
     <>
-    <div className="container">
-      <P5Sketch numerator={numerator} denominator={denominator} spacingParam={spacing} speedParam={speed}/>
-      <Slider change={(v)=>setNumerator(v)} displayValue={numerator} max="99">Numerator</Slider>
-      <Slider change={(v)=>setDenominator(v)} displayValue={denominator} max="99">Denominator</Slider>
-      <Slider change={(v)=>setSpacing(v)} displayValue={spacing} min={0} max="999">Spacing</Slider>
-      <Slider change={(v)=>setSpeed(v)} displayValue={speed} min={0} max="20">Speed</Slider>
-    </div>
+      <h1 id="title">
+        Rose curve visualizer
+      </h1>
+      <div className="visualizer">
+        <P5Sketch numerator={numerator} denominator={denominator} spacingParam={spacing} speedParam={speed} foreground={foreground} background={background} radius={18} count={10} />
+        <div className="parameters">
+          <Slider change={(v) => setNumerator(v)} displayValue={numerator} max="99">Numerator</Slider>
+          <Slider change={(v) => setDenominator(v)} displayValue={denominator} max="99">Denominator</Slider>
+          <Slider change={(v) => setSpacing(v)} displayValue={spacing} min={0} max="999">Spacing</Slider>
+          <Slider change={(v) => setSpeed(v)} displayValue={speed} min={0} max="20">Speed</Slider>
+          <ColorPicker change={(v) => setBG(v)} initialValue={arrayToHex(background)}>Background</ColorPicker>
+          <ColorPicker change={(v) => setFG(v)} initialValue={arrayToHex(foreground)}>Foreground</ColorPicker>
+          <button className="bloom-button">Bloom!</button>
+        </div>
+      </div>
     </>
   )
 }
--- src/rosecurve.jsx
+++ src/rosecurve.jsx
@@ -5,14 +5,14 @@ import { ReactP5Wrapper } from '@p5-wrapper/react'
 function P5Sketch(props) {
     const [spacing, setSpacing] = useState(0);
     const [angularSpeed, setSpeed] = useState(0);
-    const count = 10;
-    const radius = 17;
+    const count = props.count ?? 10;
+    const radius = props.radius ?? 17;
     const a = 200;
-    function sketch(p5){
+    function sketch(p5) {
         p5.setup = () => {
-            p5.createCanvas(800,800,p5.WEBGL);
+            p5.createCanvas(800, 800, p5.WEBGL);
             setSpacing(props.spacingParam);
-            setSpeed(props.speedParam * 1/p5.max(1,props.numerator-props.denominator));
+            setSpeed(props.speedParam * 1 / p5.max(1, props.numerator - props.denominator));
         }
         p5.draw = () => {
             function x(t) {
@@ -21,12 +21,12 @@ function P5Sketch(props) {
             function y(t) {
                 return a * p5.cos(props.numerator / props.denominator * t) * p5.sin(t)
             };
-    
-            p5.background(20,20,100);
+
+            p5.background(props.background);
             // p5.translate(p5.width/2,p5.height/2);
             p5.noStroke();
             p5.ellipseMode(p5.CENTER);
-            p5.fill(200,200,250);
+            p5.fill(props.foreground);
             for (var i = 0; i < count; i++) {
                 let angle = i * spacing + p5.frameCount * angularSpeed;
                 angle = angle * p5.PI / 180;
@@ -35,8 +35,8 @@ function P5Sketch(props) {
         }
     }
     return (
-      <ReactP5Wrapper sketch={sketch}/>
+        <ReactP5Wrapper sketch={sketch} />
     )
-  }
+}
 
 export default P5Sketch;
